6ed0def3aef13a214025b20ea2cb37f137ff85ed
Pavan Bokka <rampavan.bokka@gmail.com>
2026-02-10 15:16:17 +0530

    code added

--- 01-Roboshop.sh
+++ 01-Roboshop.sh
@@ -4,29 +4,23 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    INSTANCE_ID=$(
-        aws ec2 run-instances \
+    INSTANCE_ID=$( aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type "t3.micro" \
         --security-group-ids $SG_ID \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
         --query 'Instances[0].InstanceId' \
-        --output text
-    )
+        --output text )
     if [ $instance == "frontend" ]; then
-        IP=$(
-            aws ec2 describe-instances \
+        IP=$( aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text
-        )
+            --output text )
     else
-        IP=$(
-            aws ec2 describe-instances \
+        IP=$( aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query 'Reservation[].Instances[].PrivateIpAddress' \
-            --output text
-        )
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text )
     fi
 
     echo "IpAddress: $IP"
